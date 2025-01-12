68ea847d2d555ef93eb8793f761385c36b3a3def
Bob Lee <bobleehk@outlook.com>
2025-01-13 00:23:32 +0800

    added a wrapper for unmanaged memory

---
+++ efibootmgr/src/core/DisposableUnmanagedMemory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace EfiBootMgr
+{
+    internal class DisposableUnmanagedMemory : IDisposable
+    {
+        public static implicit operator IntPtr(DisposableUnmanagedMemory a) => a.Handle;
+
+        public IntPtr Handle {  get; private set; }
+        public int Size { get; private set; }
+
+        public DisposableUnmanagedMemory(int size)
+        {
+            Handle = Marshal.AllocHGlobal(size);
+            Size = size;
+        }
+
+        public DisposableUnmanagedMemory(IntPtr handle, int size)
+        {
+            Handle = handle;
+            Size = size;
+        }
+
+        public void Dispose()
+        {
+            if (Handle != IntPtr.Zero) Marshal.FreeHGlobal(Handle);
+            Handle = IntPtr.Zero;
+        }
+    }
+}
